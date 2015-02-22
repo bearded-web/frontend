266bcc1e1fe0735cc8f58c87967f73cb5e79c4d9
Dmitry Manannikov <slonoed@gmail.com>
2015-02-22 16:42:50 -0300

    build

--- app/actions/report.actions.js
+++ app/actions/report.actions.js
@@ -12,102 +12,102 @@ module.exports = {
      */
     fetchScanReports: function(scan) {
 
-        //return new Promise(function(resolve) {
-        //
-        //    dispatchReports([{
-        //        scan,
-        //        id: '1',
-        //        "type": "multi",
-        //        "multi": [
-        //            {
-        //                scan,
-        //                id: '2',
-        //                "type": "issues",
-        //                "issues": [
-        //                    {
-        //                        "severity": "medium",
-        //                        "summary": "Vulnerability in angularjs version 1.2.12",
-        //                        "desc": "Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12",
-        //                        "urls": [{ "url": "http://example.com" }],
-        //                        "extras": [
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
-        //                                "title": "Some extra title 1"
-        //                            },
-        //                            {
-        //                                "url": "http://avlidienbrunn.se/angular.txt",
-        //                                "title": "Some extra title 3"
-        //                            },
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
-        //                                "title": "Some extra title 333"
-        //                            }
-        //                        ]
-        //                    },{
-        //                        "severity": "medium",
-        //                        "summary": "222Vulnerability in angularjs version 1.2.12",
-        //                        "desc": "Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12",
-        //                        "urls": [{ "url": "http://example.com" }],
-        //                        "extras": [
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
-        //                                "title": "Some extra title 1"
-        //                            },
-        //                            {
-        //                                "url": "http://avlidienbrunn.se/angular.txt",
-        //                                "title": "Some extra title 3"
-        //                            },
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
-        //                                "title": "Some extra title 333"
-        //                            }
-        //                        ]
-        //                    },{
-        //                        "severity": "hi",
-        //                        "summary": "Vulnerability in angularjs version 1.2.12",
-        //                        "desc": 'We can run the Function constructor with controlled arguments with ["a", "alert(1)"].sort(Function).',
-        //                        "urls": [{ "url": "http://example.com" }],
-        //                        "extras": [
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
-        //                                "title": "Changelog with fix"
-        //                            },
-        //                            {
-        //                                "url": "http://avlidienbrunn.se/angular.txt",
-        //                                "title": "Vulnerability description"
-        //                            },
-        //                            {
-        //                                "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
-        //                                "title": "Source code"
-        //                            }
-        //                        ]
-        //                    }
-        //                ]
-        //            },
-        //            {
-        //                scan,
-        //                id: '3',
-        //                "type": "techs",
-        //                "techs": [
-        //                    {
-        //                        "categories": [1],
-        //                        "name": "angularjs",
-        //                        "version": "1.2.12",
-        //                        "confidence": 100
-        //                    },
-        //                    {
-        //                        "categories": [1],
-        //                        "name": "jquery",
-        //                        "version": "1.11.1",
-        //                        "confidence": 100
-        //                    }
-        //                ]
-        //            }
-        //        ]
-        //    }]);
-        //
-        //    resolve();
-        //});
+        return new Promise(function(resolve) {
+
+            dispatchReports([{
+                scan,
+                id: '1',
+                "type": "multi",
+                "multi": [
+                    {
+                        scan,
+                        id: '2',
+                        "type": "issues",
+                        "issues": [
+                            {
+                                "severity": "medium",
+                                "summary": "Vulnerability in angularjs version 1.2.12",
+                                "desc": "Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12",
+                                "urls": [{ "url": "http://example.com" }],
+                                "extras": [
+                                    {
+                                        "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
+                                        "title": "Some extra title 1"
+                                    },
+                                    {
+                                        "url": "http://avlidienbrunn.se/angular.txt",
+                                        "title": "Some extra title 3"
+                                    },
+                                    {
+                                        "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
+                                        "title": "Some extra title 333"
+                                    }
+                                ]
+                            },{
+                                "severity": "medium",
+                                "summary": "222Vulnerability in angularjs version 1.2.12",
+                                "desc": "Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12Vulnerability in angularjs version 1.2.12",
+                                "urls": [{ "url": "http://example.com" }],
+                                "extras": [
+                                    {
+                                        "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
+                                        "title": "Some extra title 1"
+                                    },
+                                    {
+                                        "url": "http://avlidienbrunn.se/angular.txt",
+                                        "title": "Some extra title 3"
+                                    },
+                                    {
+                                        "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
+                                        "title": "Some extra title 333"
+                                    }
+                                ]
+                            },{
+                                "severity": "hi",
+                                "summary": "Vulnerability in angularjs version 1.2.12",
+                                "desc": 'We can run the Function constructor with controlled arguments with ["a", "alert(1)"].sort(Function).',
+                                "urls": [{ "url": "http://example.com" }],
+                                "extras": [
+                                    {
+                                        "url": "https://github.com/angular/angular.js/blob/b3b5015cb7919708ce179dc3d6f0d7d7f43ef621/CHANGELOG.md",
+                                        "title": "Changelog with fix"
+                                    },
+                                    {
+                                        "url": "http://avlidienbrunn.se/angular.txt",
+                                        "title": "Vulnerability description"
+                                    },
+                                    {
+                                        "url": "https://github.com/angular/angular.js/commit/b39e1d47b9a1b39a9fe34c847a81f589fba522f8",
+                                        "title": "Source code"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        scan,
+                        id: '3',
+                        "type": "techs",
+                        "techs": [
+                            {
+                                "categories": [1],
+                                "name": "angularjs",
+                                "version": "1.2.12",
+                                "confidence": 100
+                            },
+                            {
+                                "categories": [1],
+                                "name": "jquery",
+                                "version": "1.11.1",
+                                "confidence": 100
+                            }
+                        ]
+                    }
+                ]
+            }]);
+
+            resolve();
+        });
 
         return scans.reports(scan)
             .then(extractor)
