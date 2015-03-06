99496ecd416f0e50331493a4945ec14030a288ea
Dmitry Manannikov <slonoed@gmail.com>
2015-03-05 22:52:26 -0300

    Make nice comment input

--- app/components/comment-form.js
+++ app/components/comment-form.js
@@ -11,34 +11,74 @@ export default React.createClass({
         onNewComment: PropTypes.func
     },
 
+    getInitialState() {
+        return {
+            expanded: false 
+        };
+    },
+
+    expand() {
+        this.setState({ expanded: true });
+
+        this.refs.text.getDOMNode().focus()
+    },
+
+    collapse() {
+        let input = this.refs.text.getDOMNode(),
+            text = input.value;
+
+        if (!text) {
+            this.setState({ expanded: false });
+        }
+    },
+
     onSubmit(event) {
-        let input = this.refs.text,
-            text = input.getValue();
+        let input = this.refs.text.getDOMNode(),
+            text = input.value;
         
         event.preventDefault();
 
         if (!text) return;
 
         this.props.onNewComment(text);
-        input.getDOMNode().childNodes[0].value = '';
+        input.value = '';
     },
 
     render: function() {
-        let { onNewComment } = this.props;
+        let { onNewComment } = this.props,
+            { expanded } = this.state,
+            style = {overflow: 'hidden'};
+
+        if (!expanded) { 
+            style.height = '0px';
+            style.margin = '0';
+        }
             
         return <form onSubmit={this.onSubmit}>
-            <Input 
-                type="textarea"
-                ref="text"
-                placeholder={iget('Enter comment text')}/>
+            {expanded || <input
+                ref="fake" 
+                type="text"
+                className="form-control" 
+                onFocus={this.expand}
+                placeholder="Enter comment"/>}
 
-            <Button 
+            <div className="form-group" style={style}>
+                <textarea 
+                    autoFocus
+                    ref="text"
+                    onBlur={this.collapse}
+                    placeholder={iget('Enter comment text')}
+                    className="form-control">
+                </textarea>
+            </div>
+
+            {expanded && <Button 
                 type="submit"
                 bsSize="small"
                 bsStyle="primary">
             
                 {iget('Add comment')}
-            </Button>
+            </Button>}
         </form>
     }
 });
