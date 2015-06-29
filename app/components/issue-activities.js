659da300a78b4a78be047616e84bed1ed88f0498
Dmitry Manannikov <slonoed@gmail.com>
2015-06-29 16:03:38 +0300

    Show user for issue activity that is created by user #119
    resolve #119

--- app/components/issue-activities.js
+++ app/components/issue-activities.js
@@ -1,23 +1,28 @@
-'use strict';
-
 import { PropTypes, Component } from 'react/addons';
 import { shouldComponentUpdate } from 'react-immutable-render-mixin';
 import { List } from 'immutable';
-import { bindAll, capitalize } from 'lodash';
+import { capitalize } from 'lodash';
+import autobind from '../lib/autobind';
+import { context } from '../lib/nf';
+import { create as createStyle } from 'react-style';
 
 import TimelineItem from './timeline-item';
 import { Link } from 'react-router';
+import Avatar from './Avatar';
 
+const cursors = { users: ['users'] };
+const S = createStyle({
+    avatar: { verticalAlign: 'bottom', margin: '0 0.5rem' }
+});
+
+@context({ cursors })
 export default class IssueActivities extends Component {
-    constructor(props, context) {
-        super(props, context);
+    static propTypes = {
+        users: PropTypes.object,
+        activities: PropTypes.instanceOf(List)
+    };
 
-        bindAll(this, [
-            'renderActivity'
-        ]);
-
-        this.shouldComponentUpdate = shouldComponentUpdate;
-    }
+    shouldComponentUpdate = shouldComponentUpdate;
 
     render() {
         const { activities } = this.props;
@@ -27,13 +32,26 @@ export default class IssueActivities extends Component {
         </div>;
     }
 
+    @autobind
     renderActivity(activity, i) {
-        const { created, type, report } = activity.toObject();
+        const { users } = this.props;
+        const { created, type, report, user: userId } = activity.toObject();
         const scan = report && report.get('scan');
+
+        let usertext = '';
+        if (type === 'reported' && userId) {
+            const user = users[userId];
+            if(user) {
+                usertext = <span>
+                    &nbsp;by <Avatar avatar={user.avatar} size={20} style={S.avatar}/>{user.nickname}
+                </span>;
+            }
+        }
 
         return <TimelineItem icon="cog" date={created} key={i}>
             <p className="m-b-xs">
                 <strong>{capitalize(type)}</strong>
+                {usertext}
             </p>
 
             {report && <p>
@@ -42,8 +60,3 @@ export default class IssueActivities extends Component {
         </TimelineItem>;
     }
 }
-
-IssueActivities.propTypes = {
-    activities: PropTypes.instanceOf(List)
-};
-
